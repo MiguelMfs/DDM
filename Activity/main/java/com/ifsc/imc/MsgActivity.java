2cb2018b504b614c43ea37d675a9ca4ea183f886
Miguel Santos <130518190+MiguelMfs@users.noreply.github.com>
2025-03-20 21:47:41 -0300

    Add files via upload

---
+++ Activity/main/java/com/ifsc/imc/MsgActivity.java
@@ -0,0 +1,29 @@
+package com.ifsc.imc;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MsgActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_msg);
+
+        textView=findViewById(R.id.TxtActivity);
+
+        Bundle bundle=getIntent().getExtras();
+        textView.setText(bundle.getString("msg"));
+
+    }
+}
