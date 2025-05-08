2ae3490afe0f5d9b9ecc64c900054b504ccb1038
Miguel Santos <130518190+MiguelMfs@users.noreply.github.com>
2025-05-08 06:53:43 -0300

    Update MainActivity.java

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -1,28 +1,8 @@
 package com.ifsc.contaclick;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.myapplication.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
