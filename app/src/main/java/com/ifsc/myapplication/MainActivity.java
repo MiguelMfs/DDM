024b29affb73b004a35b54db2c68e7d9c8173342
Miguel Santos <130518190+MiguelMfs@users.noreply.github.com>
2025-05-08 06:34:40 -0300

    Update MainActivity.java

--- app/src/main/java/com/ifsc/myapplication/MainActivity.java
+++ app/src/main/java/com/ifsc/myapplication/MainActivity.java
@@ -1,38 +1,52 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.myapplication.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-public class ActivityFormularioConstraint extends AppCompatActivity {
+import java.util.Random;
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+public class MainActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_formulario_constraint);
-        }
+    private EditText editTextMin, editTextMax;
+    private Button buttonSortear;
+    private TextView textViewResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextMin = findViewById(R.id.editTextMin);
+        editTextMax = findViewById(R.id.editTextMax);
+        buttonSortear = findViewById(R.id.buttonSortear);
+        textViewResultado = findViewById(R.id.textViewResultado);
+
+        buttonSortear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String minString = editTextMin.getText().toString();
+                String maxString = editTextMax.getText().toString();
+
+                if (!minString.isEmpty() && !maxString.isEmpty()) {
+                    int min = Integer.parseInt(minString);
+                    int max = Integer.parseInt(maxString);
+
+                    if (min > max) {
+                        textViewResultado.setText("O valor mínimo não pode ser maior que o máximo!");
+                    } else {
+                        Random random = new Random();
+                        int numeroSorteado = random.nextInt((max - min) + 1) + min;
+
+                        textViewResultado.setText("Número sorteado: " + numeroSorteado);
+                    }
+                } else {
+                    textViewResultado.setText("Por favor, preencha os dois valores.");
+                }
+            }
+        });
     }
+}
