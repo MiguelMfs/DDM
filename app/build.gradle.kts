168b08b1f56a4c6c82d49cd653f9b3a348bb719d
Miguel Santos <130518190+MiguelMfs@users.noreply.github.com>
2025-07-03 19:48:35 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.android.application)
 }
 
 android {
-    namespace = "com.ifsc.myapplication"
-    compileSdk = 34
+    namespace = "com.example.sensores"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.ifsc.myapplication"
-        minSdk = 26
-        targetSdk = 34
+        applicationId = "com.example.sensores"
+        minSdk = 24
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -26,8 +26,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -35,8 +38,11 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation ("org.osmdroid:osmdroid-android:6.1.16");
+    implementation("androidx.preference:preference:1.2.1")
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
